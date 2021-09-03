4649b08617b4f31a9262a197f15476f0c6544a1e
iarisaldy <iarisaldy@gmail.com>
2021-09-03 16:45:55 +0700

    initial commit

---
+++ project/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="wrapper">
+	  <app-sidebar v-if="isAuth"></app-sidebar>
+	    <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+				<app-header v-if="isAuth"></app-header>
+    			<Nuxt />
+			</div>
+			<app-footer v-if="isAuth"></app-footer>
+		</div>
+  </div>
+</template>
+
+<script>
+	import Sidebar from '@/components/layouts/Sidebar.vue'
+	import Header from '@/components/layouts/Header.vue'
+	import Footer from '@/components/layouts/Footer.vue'
+	import {mapState} from 'vuex'
+
+	export default {
+		data(){
+			return{
+				//isAuth:false
+			}
+		},
+		computed:{
+			...mapState(['isAuth'])
+		},
+		components:{
+			'app-sidebar' : Sidebar,
+			'app-header' : Header,
+			'app-footer' : Footer
+		}
+	}
+</script>
---
+++ project/store/index.js
@@ -0,0 +1,25 @@
+export const state = () => ({
+    isAuth:false,
+    mutations: {
+        SET_IS_AUTH(state, payload) {
+          state.isAuth = payload;
+        },
+    }
+})
+
+export const mutation = {     
+    SET_IS_AUTH(state , payload){
+    state.isAuth = payload 
+    } 
+}
+
+// export default new Vuex.Store({
+//     state: {
+//         isAuth:false
+//     },
+//     mutations: {
+//         setAuth(state, payload) {
+//         state.isAuth = payload;
+//         }
+//     }
+// })
